d0c52618b392a5f391b995815b21e1117d1433fa
tongjunhui <tongjunhui@live.cn>
2012-10-31 21:31:52 +0800

    删除无用文件，修改编译错误

--- build/diff.bat
+++
@@ -1,7 +0,0 @@
-set BC=%GS%\beyondcompare\BCompare.exe
-
-start %BC% bdlog_2005.sln bdlog_2008.sln
-start %BC% bdlog_2005.vcproj bdlog_2008.vcproj
-start %BC% testbdlog_2005.vcproj testbdlog_2008.vcproj
-start %BC% bdlogview_2005.vcproj bdlogview_2008.vcproj
-
--- build/versionnum.h
+++
@@ -1,3 +0,0 @@
-#define VERSION_INT 9,0,0,1 
-#define VERSION_STR "9.0.0.1" 
- 
